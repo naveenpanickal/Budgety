76978c8312bd5e78a3c70fd996c633094803b206
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2019-08-18 18:39:48 +0530

    Display new item to UI

--- app.js
+++ app.js
@@ -72,7 +72,9 @@ var uiController = (function(){
        inputType : ".add__type",
        inputDescrption : ".add__description",
        inputValue : ".add__value",
-       inputButton : ".add__btn"
+       inputButton : ".add__btn",
+       incomeContainer:".income__list",
+       expensesContainer:".expenses__list"
    };
     
     return {
@@ -84,7 +86,25 @@ var uiController = (function(){
     },
        getDOMstrings:function(){
            return DOMstrings;
-       }
+    },
+       addListItem:function(obj,type){
+        var html, newHtml,element;
+        //create html string with placeholder
+        if(type==="inc"){
+            element = DOMstrings.incomeContainer;
+        html ='  <div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        }
+        else if (type==="exp"){
+            element = DOMstrings.expensesContainer;
+            html= '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        }
+        //Replace the placeholder with actual data
+        newHtml = html.replace("%id%",obj.id);
+        newHtml = newHtml.replace("%description%",obj.description);
+        newHtml = newHtml.replace("%value%",obj.value);
+        //Insert HTML into the dom
+        document.querySelector(element).insertAdjacentHTML("beforeend",newHtml);
+    }
 };
 
 })();
@@ -113,7 +133,7 @@ function ctrlAddItem(){
     // 2.Transfer that data to the budget controller
     newItem = budgetCtrl.addItem(input.type,input.description,input.value);
     // 3.Update UI with new item added
-
+     UICtrl.addListItem(newItem,input.type);
     // 4.Calculate the budget
     // 5.Display the total in UI
 
